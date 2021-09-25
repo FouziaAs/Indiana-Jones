89ca87476e6d293fd44e46c92db7de365c1d291e
Fouzia <fouzia-asri_student2021@wilder.school.com>
2021-09-25 12:12:36 +0200

    indiana jones

---
+++ indiana.php
@@ -0,0 +1,15 @@
+<?php
+
+$movieName = "Indiana Jones and the Last Crusade";
+
+$wasSeen = TRUE;
+
+$releaseYear = 1989;
+
+$note = 8.2;
+
+echo $movieName;
+echo $wasSeen;
+echo $release;
+echo $note;
+?>
---
+++ indianajones.php
@@ -0,0 +1,20 @@
+<?php
+
+$movieName = "Indiana Jones and the Last Crusade";
+
+$haveYouSeen = TRUE;
+
+$yearOfRelease = 1989;
+
+$note = 8.2;
+
+echo $movieName;
+echo "<br>";
+
+echo $haveYouSeen;
+echo "<br>";
+
+echo $yearOfRelease;
+echo "<br>";
+echo $note;
+?>
